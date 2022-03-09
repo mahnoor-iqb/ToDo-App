ebf95dbd52278b6b54665b0049eb67177bf3a3b7
mahnoor-iqb <mahnoor.iqbal@emumba.com>
2022-03-09 17:12:39 +0500

    Basic DB set-up

--- models/task.py
+++ models/task.py
@@ -1,1 +1,14 @@
+from . import db, app
+from datetime import datetime
+
+# Task Schema
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(500))
+    creation_date = db.Column(db.DateTime(timezone = True), default = datetime.utcnow)
+    due_date = db.Column(db.DateTime(timezone = True))
+    completion_date = db.Column(db.DateTime(timezone = True))
+    completion_status = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
--- models/user.py
+++ models/user.py
@@ -1,1 +1,8 @@
+from . import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))   
+    tasks = db.relationship('Task')
