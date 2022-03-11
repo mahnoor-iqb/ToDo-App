9801f8e334138df54986386eb8bbff0a11b78596
mahnoor-iqb <mahnoor.iqbal@emumba.com>
2022-03-11 14:21:30 +0500

    Added methods

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -7,14 +7,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-def show():
+def show_all():
     data = User.query.all()
     return jsonify(json_list=[i.serialize for i in data])
 
+
+def show(user_id):
+    user = User.query.get(user_id)
+    return jsonify(user.serialize)
+
+
 def add():
-    name = request.form["name"]
-    email = request.form["email"]
-    password = request.form["password"]
+    name = request.json["name"]
+    email = request.json["email"]
+    password = request.json["password"]
 
     user = User(name=name, email=email, password=password)
     db.session.add(user)
--- routes/task_route.py
+++ routes/task_route.py
@@ -1,8 +1,11 @@
 
 from flask import Blueprint
 
-from controllers.task_controller import show, add
+from controllers.task_controller import show_all, add, delete,update, show
 
 task_bp = Blueprint('task_bp', __name__)
-task_bp.route('/', methods=['GET'])(show)
+task_bp.route('/', methods=['GET'])(show_all)
 task_bp.route('/', methods=['POST'])(add)
+task_bp.route('/<int:task_id>', methods=['GET'])(show)
+task_bp.route('/<int:task_id>', methods=['DELETE'])(delete)
+task_bp.route('/<int:task_id>', methods=['PUT'])(update)
